17552138febfec94cc618af636cf621d95f493c3
unni12345 <rajkrishnanv@gmail.com>
2022-05-08 16:21:29 -0400

    autograd in pytorch

---
+++ pytorch_autograd.py
@@ -0,0 +1,50 @@
+import torch
+import math
+
+dtype = torch.float
+device = torch.device("cpu")
+
+x = torch.linspace(-math.pi,math.pi, 2000)
+y = torch.sin(x)
+
+a = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+b = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+c = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+d = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+learning_rate = 1e-6
+
+for t in range(2000):
+    # forward pass calculate y_pred = a + bx + cx^2 + dx^3
+    y_pred = a + b*x + c*x**2 + d*x**3
+    
+    # Compute and print loss using operations on Tensors.
+    # Now loss is a Tensor of shape (1,)
+    # loss.item() gets the scalar value held in the loss.
+    loss = (y_pred - y).pow(2).sum()
+    if t % 100 == 99:
+        print(t, loss.item())
+
+    # backpropagation to compute the gradients of a,b,c,d w.r.t loss
+    # Use autograd to compute the backward pass. This call will compute the
+    # gradient of loss with respect to all Tensors with requires_grad=True.
+    # After this call a.grad, b.grad. c.grad and d.grad will be Tensors holding
+    # the gradient of the loss with respect to a, b, c, d respectively.
+    loss.backward()
+
+    # Update weights
+    # Manually update weights using gradient descent. Wrap in torch.no_grad()
+    # because weights have requires_grad=True, but we don't need to track this
+    # in autograd.
+    with torch.no_grad():
+        a -= learning_rate * a.grad
+        b -= learning_rate * b.grad
+        c -= learning_rate * c.grad
+        d -= learning_rate * d.grad
+
+        # Manually zero the gradients after updating weights
+        a.grad = None
+        b.grad = None
+        c.grad = None
+        d.grad = None
+
+print(f'Result: y = {a} + {b} x + {c} x^2 + {d} x^3')
